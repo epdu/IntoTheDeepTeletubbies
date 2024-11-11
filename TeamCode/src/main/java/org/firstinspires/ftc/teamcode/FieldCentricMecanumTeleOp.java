974c717875bcf5942a5a64324d60920772484419
epdu <epdu@temple.edu>
2024-11-11 17:12:40 -0500

    9-axis imu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -33,7 +32,7 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
         // Retrieve the IMU from the hardware map
         //inorder to reduce the ESD problems, we updated to be REV 9 axis imu with i2c port 1, imuinternal for the
         // REV control hub build in imu
-        //     IMU imu = hardwareMap.get(IMU.class, "imuinternal");
+        //IMU imu = hardwareMap.get(IMU.class, "imuinternal");
         IMU imu = hardwareMap.get(IMU.class, "imu");
         // Adjust the orientation parameters to match your robot
         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
@@ -50,9 +49,10 @@ public class FieldCentricMecanumTeleOp extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = gamepad1.left_stick_y; // Remember, Y stick value is reversed
-            double x = - gamepad1.left_stick_x;
-            double rx = - gamepad1.right_stick_x*(0.5);
+            // tune the power for easy to control it
+            double y = gamepad1.left_stick_y*(0.7); // Remember, Y stick value is reversed
+            double x = - gamepad1.left_stick_x*(0.7);
+            double rx = - gamepad1.right_stick_x*(0.5); //*(0.5) is fine
 
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
