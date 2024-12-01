174139dfc765bcd676161cec766246e0b0acdf5c
epdu <epdu@temple.edu>
2024-12-01 08:50:18 -0500

    clean

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FieldCentricMecanumTeleOp.java
@@ -1,88 +1,88 @@
-package org.firstinspires.ftc.teamcode;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.IMU;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-
-@TeleOp
-public class FieldCentricMecanumTeleOp extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-        // Declare our motors
-        // Make sure your ID's match your configuration
-        DcMotor LFMotor = hardwareMap.dcMotor.get("LFMotor");
-        DcMotor LBMotor = hardwareMap.dcMotor.get("LBMotor");
-        DcMotor RFMotor = hardwareMap.dcMotor.get("RFMotor");
-        DcMotor RBMotor = hardwareMap.dcMotor.get("RBMotor");
-
-
-        // Reverse the right side motors. This may be wrong for your setup.
-        // If your robot moves backwards when commanded to go forwards,
-        // reverse the left side instead.
-        // See the note about this earlier on this page.
-        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-//  why?
-//       LFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-//       LBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-        // Retrieve the IMU from the hardware map
-        //inorder to reduce the ESD problems, we updated to be REV 9 axis imu with i2c port 1, imuinternal for the
-        // REV control hub build in imu
-        //IMU imu = hardwareMap.get(IMU.class, "imuinternal");
-        IMU imu = hardwareMap.get(IMU.class, "imu");
-        // Adjust the orientation parameters to match your robot
-        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                //                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                //                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
-                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
-                RevHubOrientationOnRobot.UsbFacingDirection.DOWN));
-        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
-        imu.initialize(parameters);
-
-        waitForStart();
-
-
-        if (isStopRequested()) return;
-
-        while (opModeIsActive()) {
-            // tune the power for easy to control it
-            double y = gamepad1.left_stick_y*(0.7); // Remember, Y stick value is reversed
-            double x = - gamepad1.left_stick_x*(0.7);
-            double rx = - gamepad1.right_stick_x*(0.5); //*(0.5) is fine
-
-            // This button choice was made so that it is hard to hit on accident,
-            // it can be freely changed based on preference.
-            // The equivalent button is start on Xbox-style controllers.
-            if (gamepad1.options) {
-                imu.resetYaw();
-            }
-
-            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
-
-            // Rotate the movement direction counter to the bot's rotation
-            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
-            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
-
-            rotX = rotX * 1.1;  // Counteract imperfect strafing
-
-            // Denominator is the largest motor power (absolute value) or 1
-            // This ensures all the powers maintain the same ratio,
-            // but only if at least one is out of the range [-1, 1]
-            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
-            double frontLeftPower = (rotY + rotX + rx) / denominator;
-            double backLeftPower = (rotY - rotX + rx) / denominator;
-            double frontRightPower = (rotY - rotX - rx) / denominator;
-            double backRightPower = (rotY + rotX - rx) / denominator;
-
-            LFMotor.setPower(frontLeftPower);
-            LBMotor.setPower(backLeftPower);
-            RFMotor.setPower(frontRightPower);
-            RBMotor.setPower(backRightPower);
-        }
-    }
-
-}
+//package org.firstinspires.ftc.teamcode;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.IMU;
+//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//import com.qualcomm.robotcore.hardware.DcMotor;
+//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+//import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+//
+//@TeleOp
+//public class FieldCentricMecanumTeleOp extends LinearOpMode {
+//    @Override
+//    public void runOpMode() throws InterruptedException {
+//        // Declare our motors
+//        // Make sure your ID's match your configuration
+//        DcMotor LFMotor = hardwareMap.dcMotor.get("LFMotor");
+//        DcMotor LBMotor = hardwareMap.dcMotor.get("LBMotor");
+//        DcMotor RFMotor = hardwareMap.dcMotor.get("RFMotor");
+//        DcMotor RBMotor = hardwareMap.dcMotor.get("RBMotor");
+//
+//
+//        // Reverse the right side motors. This may be wrong for your setup.
+//        // If your robot moves backwards when commanded to go forwards,
+//        // reverse the left side instead.
+//        // See the note about this earlier on this page.
+//        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+////  why?
+////       LFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+////       LBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//
+//        // Retrieve the IMU from the hardware map
+//        //inorder to reduce the ESD problems, we updated to be REV 9 axis imu with i2c port 1, imuinternal for the
+//        // REV control hub build in imu
+//        //IMU imu = hardwareMap.get(IMU.class, "imuinternal");
+//        IMU imu = hardwareMap.get(IMU.class, "imu");
+//        // Adjust the orientation parameters to match your robot
+//        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+//                //                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+//                //                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+//                RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
+//                RevHubOrientationOnRobot.UsbFacingDirection.DOWN));
+//        // Without this, the REV Hub's orientation is assumed to be logo up / USB forward
+//        imu.initialize(parameters);
+//
+//        waitForStart();
+//
+//
+//        if (isStopRequested()) return;
+//
+//        while (opModeIsActive()) {
+//            // tune the power for easy to control it
+//            double y = gamepad1.left_stick_y*(0.7); // Remember, Y stick value is reversed
+//            double x = - gamepad1.left_stick_x*(0.7);
+//            double rx = - gamepad1.right_stick_x*(0.5); //*(0.5) is fine
+//
+//            // This button choice was made so that it is hard to hit on accident,
+//            // it can be freely changed based on preference.
+//            // The equivalent button is start on Xbox-style controllers.
+//            if (gamepad1.options) {
+//                imu.resetYaw();
+//            }
+//
+//            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+//
+//            // Rotate the movement direction counter to the bot's rotation
+//            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+//            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+//
+//            rotX = rotX * 1.1;  // Counteract imperfect strafing
+//
+//            // Denominator is the largest motor power (absolute value) or 1
+//            // This ensures all the powers maintain the same ratio,
+//            // but only if at least one is out of the range [-1, 1]
+//            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+//            double frontLeftPower = (rotY + rotX + rx) / denominator;
+//            double backLeftPower = (rotY - rotX + rx) / denominator;
+//            double frontRightPower = (rotY - rotX - rx) / denominator;
+//            double backRightPower = (rotY + rotX - rx) / denominator;
+//
+//            LFMotor.setPower(frontLeftPower);
+//            LBMotor.setPower(backLeftPower);
+//            RFMotor.setPower(frontRightPower);
+//            RBMotor.setPower(backRightPower);
+//        }
+//    }
+//
+//}
