8ac77dd535c3dbe9ca2f5f9ee34018e98e984249
epdu <epdu@temple.edu>
2024-12-12 00:47:40 -0500

    tune Vslides

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeepTeleOpTeletubbies.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeepTeleOpTeletubbies.java
@@ -37,6 +37,7 @@ public class IntoTheDeepTeleOpTeletubbies extends LinearOpMode {
         while (opModeIsActive()) {
 
             moveDriveTrain();
+            liftVertSlidesHigh();
 //Begin Definition and Initialization of gamepad
 
 //                        if (gamepad1.left_trigger > 0.3) { //open
