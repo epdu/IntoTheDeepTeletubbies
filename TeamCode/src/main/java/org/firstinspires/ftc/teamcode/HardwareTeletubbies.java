08ad8c36f2d2b199c5dedd039d61df6133025dd9
epdu <epdu@temple.edu>
2024-12-13 03:46:42 -0500

    Expansion port 1 output is reversed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTeletubbies.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareTeletubbies.java
@@ -1,72 +1,74 @@
 
-       package org.firstinspires.ftc.teamcode;
-       import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
-       import com.qualcomm.robotcore.hardware.DcMotor;
-       import com.qualcomm.robotcore.hardware.DcMotorSimple;
-       import com.qualcomm.robotcore.hardware.HardwareMap;
-       import com.qualcomm.robotcore.hardware.IMU;
-       import com.qualcomm.robotcore.hardware.Servo;
-       import com.qualcomm.robotcore.util.ElapsedTime;
-       import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-       /**
-        * This is NOT an opmode.
-        *
-        * This class can be used to define all the specific hardware for a single robot.
-        * In this case that robot is powerpuffs`s robot from Pushbot.
-        * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
-        *
-        * This hardware class assumes the following device names have been configured on the robot:
-        * Note:  All names are lower case and some have single spaces between words.
-        *    DcMotor RFMotor;
-        *    DcMotor LFMotor;
-        *    DcMotor RBMotor;
-        *    DcMotor LBMotor;
-        * RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
-        * LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
-        * RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
-        * LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
-        * Motor channel:  Left  drive motor:        "left_drive"
-        * Motor channel:  Right drive motor:        "right_drive"
-        * Motor channel:  Manipulator drive motor:  "left_arm"
-        * Servo channel:  Servo to open left claw:  "left_hand"
-        * Servo channel:  Servo to open right claw: "right_hand"
-        *
-        *
-        */
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+/**
+ * This is NOT an opmode.
+ *
+ * This class can be used to define all the specific hardware for a single robot.
+ * In this case that robot is powerpuffs`s robot from Pushbot.
+ * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+ *
+ * This hardware class assumes the following device names have been configured on the robot:
+ * Note:  All names are lower case and some have single spaces between words.
+ *    DcMotor RFMotor;
+ *    DcMotor LFMotor;
+ *    DcMotor RBMotor;
+ *    DcMotor LBMotor;
+ * RFMotor = hardwareMap.get(DcMotor.class, "RFMotor");
+ * LFMotor = hardwareMap.get(DcMotor.class, "LFMotor");
+ * RBMotor = hardwareMap.get(DcMotor.class, "RBMotor");
+ * LBMotor = hardwareMap.get(DcMotor.class, "LBMotor");
+ * Motor channel:  Left  drive motor:        "left_drive"
+ * Motor channel:  Right drive motor:        "right_drive"
+ * Motor channel:  Manipulator drive motor:  "left_arm"
+ * Servo channel:  Servo to open left claw:  "left_hand"
+ * Servo channel:  Servo to open right claw: "right_hand"
+ *
+ *
+ */
 //Good version 02082024
-       public class HardwareTeletubbies
-       {
-           /* local OpMode members. */
-           HardwareMap hwMap =  null;
-           /* Public OpMode members. */
-           public DcMotor RFMotor;
-           public DcMotor LFMotor;
-           public DcMotor RBMotor;
-           public DcMotor LBMotor;
-           public Servo IClaw;
-           public Servo OClaw;
-           public Servo Wrist;
-           public Servo Wristzyaw;
-           public Servo Wristxpitch;
-/*
-       x right side of the robot
-       y forward
-       z celling
-       z--yaw
-       x--pitch
-       y-- roll
-*/
-           public Servo IArmL;
-           public Servo IArmR;
-           public Servo OArmL;
-           public Servo OArmR;
-           public DcMotor HSMotor; //horizontal Slides motor  extruder
-           public DcMotor VSMotorL; //vertical Slides motor left
-           public DcMotor VSMotorR; //vertical Slides motor right
-           public Servo TServo; // For testing
+public class HardwareTeletubbies
+{
+    /* local OpMode members. */
+    HardwareMap hwMap =  null;
+    /* Public OpMode members. */
+    public DcMotor RFMotor;
+    public DcMotor LFMotor;
+    public DcMotor RBMotor;
+    public DcMotor LBMotor;
+    public Servo IClaw;
+    public Servo OClaw;
+    public Servo Wrist;
+    public Servo Wristzyaw;
+    public Servo Wristxpitch;
+    /*
+           x right side of the robot
+           y forward
+           z celling
+           z--yaw
+           x--pitch
+           y-- roll
+    */
+    public Servo IArmL;
+    public Servo IArmR;
+    public Servo OArmL;
+    public Servo OArmR;
+    public DcMotor HSMotor; //horizontal Slides motor  extruder
+    public DcMotor VSMotorL; //vertical Slides motor left
+    public DcMotor VSMotorR; //vertical Slides motor right
+    public Servo TServo; // For testing
 //           public DcMotor TMotor; // For testing
 
-           //           public Servo ArmL;
+
+
+    //           public Servo ArmL;
 //           public Servo ArmR;
 //           public Servo V4BL;
 //           public Servo V4BR;
@@ -78,40 +80,40 @@
 //           public Servo ArmR;
 //           public Servo ArmL;
 //           public Servo Drone;
-           IMU imu;
-           public static final double DriveTrains_POWER       =  0.5 ;
-           public static final double MID_SERVO       =  0.5 ;
-           public static final double ARM_UP_POWER    =  0.45 ;
-           public static final double ARM_DOWN_POWER  = -0.45 ;
-           private ElapsedTime period  = new ElapsedTime();
-           /* Constructor */
-           /* Initialize standard Hardware interfaces */
-           public void init(HardwareMap ahwMap) {
-               // Save reference to Hardware map
-               hwMap = ahwMap;
+    IMU imu;
+    public static final double DriveTrains_POWER       =  0.5 ;
+    public static final double MID_SERVO       =  0.5 ;
+    public static final double ARM_UP_POWER    =  0.45 ;
+    public static final double ARM_DOWN_POWER  = -0.45 ;
+    private ElapsedTime period  = new ElapsedTime();
+    /* Constructor */
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
 
 //Begin Definition and Initialization of Drivetrain Motors
-               LFMotor   = hwMap.get(DcMotor.class, "LFMotor");//02022024 control hub port 0
-               RFMotor  = hwMap.get(DcMotor.class, "RFMotor"); //02022024 control hub port 1
-               LBMotor   = hwMap.get(DcMotor.class, "LBMotor");//02022024 control hub port 2
-               RBMotor  = hwMap.get(DcMotor.class, "RBMotor");//02022024 control hub port 3
+        LFMotor   = hwMap.get(DcMotor.class, "LFMotor");//02022024 control hub port 0
+        RFMotor  = hwMap.get(DcMotor.class, "RFMotor"); //02022024 control hub port 1
+        LBMotor   = hwMap.get(DcMotor.class, "LBMotor");//02022024 control hub port 2
+        RBMotor  = hwMap.get(DcMotor.class, "RBMotor");//02022024 control hub port 3
 
-               RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-               RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RBMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        RFMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
-               LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-               RFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-               LBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-               RBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RFMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        LBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        RBMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-               // Set all motors to run without encoders.
-               // May want to use RUN_USING_ENCODERS if encoders are installed.
-               LFMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-               RFMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-               LBMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-               RBMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        // Set all motors to run without encoders.
+        // May want to use RUN_USING_ENCODERS if encoders are installed.
+        LFMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RFMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LBMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RBMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
- // End Definition and Initialization of Drivetrain Motors
+        // End Definition and Initialization of Drivetrain Motors
 /*
  Expansion port 1 output is reversed
  Expansion port 1 output is reversed
@@ -120,29 +122,29 @@
 
 //Begin Definition and Initialization of Horizontal Slides  Motor
 
-               HSMotor = hwMap.get(DcMotor.class, "HSMotor");// expansion  hub  port 3
-               int positionH = HSMotor.getCurrentPosition();
-               HSMotor.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        HSMotor = hwMap.get(DcMotor.class, "HSMotor");// expansion  hub  port 3
+        int positionH = HSMotor.getCurrentPosition();
+        HSMotor.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
 
 //End Definition and Initialization of Horizontal Slides  Motor
 
 
 //Begin Definition and Initialization of Vertical Slides Motors
 
-               VSMotorL = hwMap.get(DcMotor.class, "VSMotorL");// expansion  hub port 0
-               int positionVL = VSMotorL.getCurrentPosition();
-               VSMotorL.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
-               VSMotorL.setDirection(DcMotorSimple.Direction.REVERSE);
-               VSMotorR = hwMap.get(DcMotor.class, "VSMotorR");// expansion  hub port 2
-               int positionVR = VSMotorR.getCurrentPosition();
-               VSMotorR.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        VSMotorL = hwMap.get(DcMotor.class, "VSMotorL");// expansion  hub port 0
+        int positionVL = VSMotorL.getCurrentPosition();
+        VSMotorL.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
+        VSMotorL.setDirection(DcMotorSimple.Direction.REVERSE);
+        VSMotorR = hwMap.get(DcMotor.class, "VSMotorR");// expansion  hub port 1
+        int positionVR = VSMotorR.getCurrentPosition();
+        VSMotorR.setZeroPowerBehavior((DcMotor.ZeroPowerBehavior.BRAKE));
 
 
 
 //End Definition and Initialization of Vertical Slides Motors
 
-               // Set all motors to zero power
-               setAllPower(0);
+        // Set all motors to zero power
+        setAllPower(0);
 
 //Begin Definition and Initialization of Testing Motors and Servos
 
@@ -172,8 +174,8 @@
 
 //Begin Definition and Initialization of Wristxpitch Servo
 
-//               Wristxpitch = hwMap.get(Servo.class, "Wristxpitch");//control hub port x
-//               Wristxpitch.setPosition(0.05);// wrist good
+        Wristxpitch = hwMap.get(Servo.class, "Wristxpitch");//control hub port x
+        Wristxpitch.setPosition(0.05);// wrist good
 
 //End Definition and Initialization of Wrist Servo
 
@@ -244,13 +246,13 @@
 // REV control hub build in imu
 
 
-               imu = hwMap.get(IMU.class, "imu");
-               IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-                       RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
-                       RevHubOrientationOnRobot.UsbFacingDirection.DOWN));
-               double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
+        imu = hwMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.RIGHT,
+                RevHubOrientationOnRobot.UsbFacingDirection.DOWN));
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.DEGREES);
 
-               imu.initialize(parameters);
+        imu.initialize(parameters);
 //        parameters.angleUnit           = IMU.AngleUnit.DEGREES;
 //        parameters.accelUnit           = IMU.AccelUnit.METERS_PERSEC_PERSEC;
 //        parameters.calibrationDataFile = "IMUCalibration.json"; // see the calibration sample opmode
@@ -258,21 +260,21 @@
 //        parameters.loggingTag          = "IMU";
 //        parameters.imuOrientationOnRobot = new JustLoggingAccelerationIntegrator();
 
-               // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
-               // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
-               // and named "imu".
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
 
-           }
-           //Set power to all motors
+    }
+    //Set power to all motors
 
-           public void setMotorPower(double lF, double rF, double lB, double rB){
-               LFMotor.setPower(lF*DriveTrains_POWER);
-               LBMotor.setPower(lB*DriveTrains_POWER);
-               RBMotor.setPower(rB*DriveTrains_POWER);
-               RFMotor.setPower(rF*DriveTrains_POWER);
-           }
-           public void setAllPower(double p){
-               setMotorPower(p,p,p,p);
-           }
-       }
+    public void setMotorPower(double lF, double rF, double lB, double rB){
+        LFMotor.setPower(lF*DriveTrains_POWER);
+        LBMotor.setPower(lB*DriveTrains_POWER);
+        RBMotor.setPower(rB*DriveTrains_POWER);
+        RFMotor.setPower(rF*DriveTrains_POWER);
+    }
+    public void setAllPower(double p){
+        setMotorPower(p,p,p,p);
+    }
+}
 
