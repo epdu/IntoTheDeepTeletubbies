465f433fe887c6991c4659b70563948135d286c0
epdu <epdu@temple.edu>
2024-12-14 23:36:43 -0500

    12142024 outtake tested

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeepTeleOpTeletubbies.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntoTheDeepTeleOpTeletubbies.java
@@ -25,9 +25,10 @@ public class IntoTheDeepTeleOpTeletubbies extends LinearOpMode {
     private static final int POSITION_B_EXTRUDE_MORE = 400; //horizontal slides all the way out
     private static final int POSITION_A_BOTTOM = 0; //Vertical  slides all the way in
     private static final int POSITION_Y_LOW = 800; // Vertical slides up //800 //1000 too high
-    private static final int POSITION_Y_HIGH = 1000;//Vertical slides all the way up
+    private static final int POSITION_Y_HIGH = 1100;//Vertical slides all the way up
+    private static final int POSITION_Y_HIGHH = 1200;//Vertical slides all the way up
     private static final double SLIDE_POWER_H = 0.4; // Adjust as needed
-    private static final double SLIDE_POWER_V = 0.55; // Adjust as needed
+    private static final double SLIDE_POWER_V = 0.70; // Adjust as needed
     private static final double SERVO_STEP = 0.01; // 每次调整的伺服步长
     double servoPosition = 0.5;
     private static final double SLIDE_POWER = 0.8; // Adjust as needed
@@ -52,13 +53,16 @@ public class IntoTheDeepTeleOpTeletubbies extends LinearOpMode {
             if (gamepad2.a && !move ) { //down
                 moveVSlideToPosition(POSITION_A_BOTTOM);
             }
-            if (gamepad2.y && !move) { //upforchameber
+            if (gamepad2.y && !move) { //up prepare forchameber
                 moveVSlideToPosition(-POSITION_Y_LOW);
             }
-            if (gamepad2.left_bumper && !move){ //upforchameber
+            if (gamepad2.right_bumper && !move){ //upforchameber
                 moveVSlideToPosition(-POSITION_Y_HIGH);
-            }// d  moveVSlideToPosition
-//End  moveVSlideToPosition
+            }
+            if (gamepad2.left_bumper && !move){ //upforchameber
+                moveVSlideToPosition(-POSITION_Y_HIGHH);
+            }
+            //End  moveVSlideToPosition
 //Begin  moveHSlideToPosition
             if (gamepad2.b && !move) { //in
                 moveHSlideToPosition(POSITION_X_IN);
